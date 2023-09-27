ee77e6572060d7c0e8a31104d5a8252413d30497
Francisco Mateus <fmfr@outlook.com>
2023-09-27 18:16:08 -0300

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import theme from './src/theme'
 
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
+import { Loading } from "@components/Loading";
 import { Groups } from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-        <Groups />
+        <StatusBar 
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+        { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   )
 }
