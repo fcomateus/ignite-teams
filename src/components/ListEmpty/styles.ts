4489c6bdc682a4ad5a373967c870e0c7ad1ceda1
Francisco Mateus <fmfr@outlook.com>
2023-10-05 18:41:51 -0300

    button component

---
+++ src/components/ListEmpty/styles.ts
@@ -0,0 +1,15 @@
+import styled, { useTheme } from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Message = styled.Text`
+  text-align: center;
+
+  font-size: ${() => useTheme().FONT_SIZE.SM}px;
+  font-family: ${() => useTheme().FONT_FAMILY.REGULAR};
+  color: ${() => useTheme().COLORS.GRAY_300};
+`;
