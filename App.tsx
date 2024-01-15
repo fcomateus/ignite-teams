4b45dcc24ddf338316d815ad36b023fa06532e7a
mateus <fmfr@outlook.com>
2024-01-15 09:07:23 -0300

    start doing navigation

--- App.tsx
+++ App.tsx
@@ -5,9 +5,8 @@ import theme from './src/theme'
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 import { Loading } from "@components/Loading";
-import { Groups } from "@screens/Groups";
-import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Players";
+
+import { Routes } from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -19,7 +18,7 @@ export default function App() {
           backgroundColor='transparent'
           translucent
         />
-        { fontsLoaded ? <Players /> : <Loading /> }
+        { fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   )
 }
