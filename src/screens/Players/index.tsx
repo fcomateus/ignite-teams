91e5da04ff7405fc75902d16af7ef5e0d2f2a0ca
mateus <fmfr@outlook.com>
2024-01-22 09:12:53 -0300

    listing players by group and team

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,18 +1,22 @@
-import { Header } from "@components/Header";
-import { ButtonIcon } from "@components/ButtonIcon";
-import { Container, Form, HeaderList, QuantityOfPlayers } from "./styles";
-import { Highlight } from "@components/Highlight";
-import { Input } from "@components/Input";
-import { Filter } from "@components/Filter";
-import { PlayerCard } from "@components/PlayerCard";
-import { Alert, FlatList } from "react-native";
-import { useState } from "react";
-import { ListEmpty } from "@components/ListEmpty";
-import { Button } from "@components/Button";
 import { useRoute } from "@react-navigation/native";
+import { useState, useEffect, useRef } from "react";
+import { Alert, FlatList, TextInput } from "react-native";
+
+import { Container, Form, HeaderList, QuantityOfPlayers } from "./styles";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { PlayerCard } from "@components/PlayerCard";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { Filter } from "@components/Filter";
+import { Header } from "@components/Header";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+
 import { AppError } from "@utils/AppError";
+
 import { PlayerAddByGroup } from "@storage/player/playerAddByGroup";
-import { playersGetByGroup } from "@storage/player/playersGetByGroup";
+import { playersGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam";
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 
 type RouteParams = {
     group: string
@@ -20,8 +24,10 @@ type RouteParams = {
 
 export function Players() {
     const [team, setTeam] = useState('Time A')
-    const [players, setPlayers] = useState([])
+    const [players, setPlayers] = useState<PlayerStorageDTO[]>([])
     const [newPlayerName, setNewPlayerName] = useState('')
+
+    const newPlayerNameInputRef = useRef<TextInput>(null)
 
     const route = useRoute()
     const { group } = route.params as RouteParams
@@ -38,8 +44,12 @@ export function Players() {
 
         try {
             await PlayerAddByGroup(newPlayer, group)
-            const players = await playersGetByGroup(group)
-            console.log(players);
+
+            newPlayerNameInputRef.current?.blur()
+            
+
+            setNewPlayerName('')
+            fetchPlayersByTeam()
             
         } catch (error) {
             if(error instanceof AppError) {
@@ -52,6 +62,20 @@ export function Players() {
 
     }
 
+    async function fetchPlayersByTeam() {
+        try {
+            const playersByTeam = await playersGetByGroupAndTeam(group, team)
+            setPlayers(playersByTeam)
+        } catch(error) {
+            console.log(error);
+            Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado')
+        }
+    }
+
+    useEffect(() => {
+        fetchPlayersByTeam()
+    }, [team])
+
     return (
         <Container>
             <Header showBackButton />
@@ -63,9 +87,13 @@ export function Players() {
 
             <Form>
                 <Input
+                    inputRef={newPlayerNameInputRef}
                     onChangeText={setNewPlayerName}
+                    value={newPlayerName}
                     placeholder="Nome da pessoa"
                     autoCorrect={false}
+                    onSubmitEditing={handleAddPlayer}
+                    returnKeyType="done"
                 />
                 <ButtonIcon
                     onPress={handleAddPlayer}
@@ -95,10 +123,10 @@ export function Players() {
 
             <FlatList
                 data={players}
-                keyExtractor={item => item}
+                keyExtractor={item => item.name}
                 renderItem={({ item }) => (
                     <PlayerCard 
-                        name={item}
+                        name={item.name}
                         onRemove={() => {}}
                     />
                 )}
